dd17e192a6b8067d24073b934ff2a4fcca215a3c
Fua <fuadash@protonmail.com>
2025-08-04 10:51:26 +0100

    actually fixed the directory

---
+++ core/load_data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+def load_data():
+    """
+    Load the Pokémon dataset.
+    returns a DataFrame with the Pokémon data.
+    """
+    df = pd.read_csv("data/pokemon.csv")
+    df = df.fillna("N/A")
+    return df
+
+
+if __name__ == "__main__":
+    print(load_data())
