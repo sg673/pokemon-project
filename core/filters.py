954ddeb395ee9e04fd925d930171f77f6629bb6a
Fua <fuadash@protonmail.com>
2025-08-04 11:39:30 +0100

    wrote filters and tests for single types

--- core/filters.py
+++ core/filters.py
@@ -1,16 +1,19 @@
-import load_data as ld
 import pandas as pd
-
-df = ld.load_data()
-
-print(df)
 
 
 # Filter by type
-def filter_by_type(df, type: str) -> pd.DataFrame:
-    return df
+def filter_by_type(df: pd.DataFrame, type: str) -> pd.DataFrame:
+    return df[(df["type_1"] == type) | (df["type_2"] == type)]
+
+# Filter to matching two types
+# Should this be consolidated into the previous function?
+def filter_by_dual_type(df: pd.DataFrame,
+                        type_1: str,
+                        type_2: str) -> pd.DataFrame:
+    return df[(((df["type_1"] == type_1) & (df["type_2"] == type_2)
+                | (df["type_1"] == type_2) & (df["type_2"] == type_1)))]
 
 
 # Filter by status (Legendary/Mythical)
 def filter_by_status(df, status: str) -> pd.DataFrame:
-    return df
+    return df[(df["status"] == status)]
